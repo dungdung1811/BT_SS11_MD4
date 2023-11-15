f6e63b7e9591a270ad47a1a0f488dfeb2b03687b
dungdung1811 <nguyendung181192@gmail.com>
2023-11-15 08:12:51 +0700

    BT_SS11

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(Calculator.mul(10,20));
+        System.out.println(Calculator.sum(10,20));
+        System.out.println(Calculator.sub(10,20));
+
+    }
+}
